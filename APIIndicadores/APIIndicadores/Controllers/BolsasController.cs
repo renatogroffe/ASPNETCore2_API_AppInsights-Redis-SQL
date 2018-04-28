83629241df38fef2ed67488ba82dde55a80a1e56
Renato Groffe <renato.groffe@yahoo.com.br>
2018-04-27 23:53:57 -0300

    ASP.NET Core 2.0 + Application Insights + Redis + SQL Server
    ASP.NET Core 2.0 + Application Insights + Redis + SQL Server

---
+++ APIIndicadores/APIIndicadores/Controllers/BolsasController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using APIIndicadores.Models;
+using APIIndicadores.Data;
+
+namespace APIIndicadores.Controllers
+{
+    [Route("api/[controller]")]
+    public class BolsasController : Controller
+    {
+        [HttpGet]
+        public List<BolsaValores> Get(
+            [FromServices]ApplicationDbContext context)
+        {
+            return (from b in context.BolsasValores
+                    select b).ToList();
+        }
+    }
+}
---
+++ APIIndicadores/APIIndicadores/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using APIIndicadores.Data;
+
+namespace APIIndicadores
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configurando o uso do Entity Framework Core
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("BaseIndicadores")));
+
+            // Ativando o uso de cache via Redis
+            services.AddDistributedRedisCache(options =>
+            {
+                options.Configuration =
+                    Configuration.GetConnectionString("CacheRedis");
+                options.InstanceName = "Cache-APIIndicadores-";
+            });
+
+            services.AddMvc();
+
+            // Ativando o Application Insights
+            services.AddApplicationInsightsTelemetry(Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
